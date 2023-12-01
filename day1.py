66c071c0d6ce5bccf9ff65e8c2c2e6fbfb79d40e
Niels Kouwenhoven <nkouwenhoven@meyn.com>
2023-12-01 12:43:17 +0100

    Day 1
    with oneliners

--- day1.py
+++ day1.py
@@ -30,11 +30,12 @@ def recalibrate(line):
     digits = [digitmap.get(digit, digit) for digit in digitlikes]
     return int(digits[0] + digits[-1])
 
-
+# examples
 print(sum(map(calibrate, example)))
 print(sum(map(recalibrate, example2)))
+# input data
 print(sum(map(calibrate, open("day1.txt"))))
 print(sum(map(recalibrate, open("day1.txt"))))
-
-if __name__ == '__main__':
-    pass
+# oneliners
+print(10*sum(int(next(c for c in line if c.isdigit())) for line in open("day1.txt")) + sum(int(next(c for c in reversed(line) if c.isdigit())) for line in open("day1.txt")))
+print(10 * sum(min((line.find(digit) % 99, num) for digit, num in {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '0':0}.items())[1] for line in open("day1.txt")) + sum(        max((line.rfind(digit), num) for digit, num in {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '0':0}.items())[1] for line in open("day1.txt")))
